ae248719950b055b568d457e3be12e22b9a00c5f
Isaac Awonugba <it.awonugba@gmail.com>
2023-11-06 17:50:33 +0000

    Documentation completed

---
+++ webpack.common.js
@@ -0,0 +1,23 @@
+const path = require('path');
+module.exports = {
+  entry: {
+    'cart-drawer': './src/cart-drawer.jsx'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'assets')
+  }
+};
